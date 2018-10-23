dea3fa516550904e68d46fa7d7b5d13f60e73084
Patryk Ptasinski <patryk@ipepe.pl>
2018-10-24 00:48:46 +0200

    Less Dockerfile layers. Better run-certbot.sh

--- src/run-certbot.sh
+++ src/run-certbot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$LETSENCRYPT_EMAIL" = "example@example.org" ]; then
-    certbot --nginx --agree-tos --register-unsafely-without-email -d "$VIRTUAL_HOSTS"
+    certbot --nginx --agree-tos --non-interactive --register-unsafely-without-email -d "$VIRTUAL_HOSTS"
 else
-    certbot --nginx --agree-tos -m "$LETSENCRYPT_EMAIL" -d "$VIRTUAL_HOSTS"
+    certbot --nginx --agree-tos --non-interactive -m "$LETSENCRYPT_EMAIL" -d "$VIRTUAL_HOSTS"
 fi
